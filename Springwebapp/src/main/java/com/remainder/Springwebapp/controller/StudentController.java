d295d1033bf6b78602f11337d4bc2d41a8dbfa56
moni <monikas.is19@rvce.edu.in>
2022-02-09 23:44:03 +0530

    post mapping

--- Springwebapp/src/main/java/com/remainder/Springwebapp/controller/StudentController.java
+++ Springwebapp/src/main/java/com/remainder/Springwebapp/controller/StudentController.java
@@ -1,3 +1,22 @@
 package com.remainder.Springwebapp.controller;
+
+import com.remainder.Springwebapp.model.Student;
+import com.remainder.Springwebapp.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New student added";
+    }
 }
