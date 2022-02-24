0d71b731d66a8f3cc6f133d632fa3dd517e988fa
moni <monikas.is19@rvce.edu.in>
2022-02-24 18:51:30 +0530

    maxscore method impl

--- Springwebapp/src/main/java/com/remainder/Springwebapp/service/StudentServiceImpl.java
+++ Springwebapp/src/main/java/com/remainder/Springwebapp/service/StudentServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Comparator;
 
 @Service
 public class StudentServiceImpl implements StudentService{
@@ -20,5 +21,26 @@ public class StudentServiceImpl implements StudentService{
     @Override
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
+    }
+    @Override
+    public  String maxScore(){
+        List<Student> students=studentRepository.findAll();
+        Student user1 = students.stream()
+                .min(Comparator.comparingInt(Student::getTotal))
+                .get();
+        System.out.println("Student with minimum score: " + user1.getName());
+        Student user2 = students.stream()
+                .max(Comparator.comparingInt(Student::getTotal))
+                .get();
+
+        System.out.println("Student with maximum score: " + user2.getName());
+        Student user3 = students.stream()
+                .max(Comparator.comparingInt(Student::getMath))
+                .get();
+
+        System.out.println("Student with maximum math score: " + user3.getName());
+        //Student student= new Student();
+        //return studentRepository.findById(student.getId()).get();
+        return "maxscore";
     }
 }
